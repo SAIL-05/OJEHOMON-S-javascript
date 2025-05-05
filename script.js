7118a78d7f862a72d9721527b7403a193675b87a
Ojele45 <oojehomon72@gmail.com>
2025-05-05 17:12:53 +0100

    my java script

--- script.js
+++ script.js
@@ -155,3 +155,67 @@ else {
     let number = 10;
     let isEven = (number % 2 == 0) ? "Even" : "Odd";
     console.log(isEven); // Even
+
+// conditionals statement
+// if statement 
+if (true) {
+    console.log("This is true");    
+}
+let o = 30;
+let p = 20;
+if (o > p) {
+    console.log("a is greater than b");    
+}
+
+// // if else statement
+if (o < p) {
+    console.log("a is less than b");    
+}
+else {
+    console.log("a is not less than b");    
+}
+let Dbpass = "h;fjovsdffvb";
+let password = "h;fjovsdffvb";
+
+if (Dbpass === password) {
+    console.log("Login successful");    
+}
+else {
+    console.log("Login failed");
+}
+
+let name = prompt("Enter your name: ");
+alert(name);  
+
+let mnum = prompt("Enter a number: ");
+const mmnum = prompt("Enter a number: ");
+alert34(`Your lucky number is ${num}`);
+const username = prompt("Enter your name: ");
+const mage = prompt("Enter your age: ");
+alert(`Hello ${username}, you are ${age} years old`);
+console.log(typeof mmnum);
+let valueOne = parseInt(prompt("Enter first number: "));
+let valueTwo = parseInt(prompt("Enter second number: "));
+let ans = valueOne + valueTwo;
+alert(ans);
+
+let num = parseInt(prompt("Enter number: "));
+
+if (num < 0 ){
+    alert(` ${num} is negative`);    
+}
+else if (num > 0) {
+    alert(`${num} is positive`);    
+}
+else if (num === 0) {
+    alert(`${num} is zero`);    
+}
+
+// write a program to check if a number is even or odd
+ num = parseInt(prompt("Enter number: "));
+if (num % 2 === 0) {
+    alert(`${num} is even`);
+}
+else if (num % 2 !== 0) {
+    alert(`${num} is odd`);    
+}
